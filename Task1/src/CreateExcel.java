1d2bf46eb2073afd2b64e8914a3992720e656d13
Lenovo <Lenovo@DESKTOP-RV8V8HE>
2018-07-31 10:25:29 +0800

    2018.7.31 - 1

--- Task1/src/CreateExcel.java
+++ Task1/src/CreateExcel.java
@@ -1,3 +1,5 @@
+import java.io.File;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -5,16 +7,76 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+import jxl.Workbook;
+import jxl.write.Label;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+
 public class CreateExcel {
 	
-	public static void create_excel(String sql)
+	public void create_excel(String sql)
 	{
+		WritableWorkbook workbook = null;
+		String fileName = "temp.xls";
+		File file = new File(fileName);
+		try {
+//			if (file.exists())
+//			{
+//				workbook = Workbook.createWorkbook(file, Workbook.getWorkbook(file));
+//			}
+//			else
+//			{
+//				file.createNewFile();
+//				workbook = Workbook.createWorkbook(file);
+//			}
+			file.createNewFile();
+			workbook = Workbook.createWorkbook(file);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		Connection conn = getConn();
 		try {
 			PreparedStatement ps = (PreparedStatement)conn.prepareStatement(sql);
 			ResultSet rs = (ResultSet) ps.executeQuery();
-			String[] titles;
-		} catch (SQLException e) {
+			ResultSetMetaData rsmd = rs.getMetaData();
+			
+			WritableSheet s = null;
+			int sheetCount = workbook.getNumberOfSheets();
+			s = workbook.createSheet("Sheet" + sheetCount, sheetCount);
+			for (int i = 0; i < rsmd.getColumnCount(); i++)
+			{
+				Label label = new Label(i, 0, rsmd.getColumnName(i + 1));
+                s.addCell(label);
+			}
+			int rowCount = 1;
+			while(rs.next()) {
+				for (int i = 0; i < rsmd.getColumnCount(); i++)
+				{
+					s.addCell(new Label(i, rowCount, rs.getString(i + 1)));
+				}
+				rowCount++;
+			}
+			
+			workbook.write();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				workbook.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+        }
+		
+		System.out.println(file.getPath());
+		System.out.println(file.getAbsolutePath());
+		try {
+			System.out.println(file.getCanonicalPath());
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -22,8 +84,8 @@ public class CreateExcel {
 	}
 	
 	private static Connection getConn() {
-	    String driver = "com.mysql.jdbc.Driver";
-	    String url = "jdbc:mysql://localhost:3306/test";
+	    String driver = "com.mysql.cj.jdbc.Driver";
+	    String url = "jdbc:mysql://localhost:3306/test?useSSL=true&serverTimezone=GMT%2B8";
 	    String username = "root";
 	    String password = "123456";
 	    Connection conn = null;
@@ -38,6 +100,9 @@ public class CreateExcel {
 	    return conn;
 	}
 	
-	
+	public static void main(String[] args)
+	{
+		new CreateExcel().create_excel("SELECT * FROM table_1;");
+	}
 
 }
